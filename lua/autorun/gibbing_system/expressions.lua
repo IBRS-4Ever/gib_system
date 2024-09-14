3421cb27341609d09d21549cdcc2f329986e8e22
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2024-09-14 18:01:28 +0800

    Update expressions.lua

--- lua/autorun/gibbing_system/expressions.lua
+++ lua/autorun/gibbing_system/expressions.lua
@@ -9,7 +9,7 @@ function GibFacePose(ent)
 		
 		if ModelExpressions then 																-- 如果 ModelExpressions 表中有该模型的表情值，则从表中读取。
 			Expressions = ModelExpressions[math.random(1, table.Count(ModelExpressions))]
-		elseif list.HasEntry( "GIBSYSTEM_GIRLS_FRONTLINE_2_MODELS", Model ) then 				-- 如果是少前2的模型，则套用以下值。
+		elseif GIRLS_FRONTLINE_2_MODELS[Model] then 											-- 如果是少前2的模型，则套用以下值。
 			Expressions = {
 				["eye_blink_left"] = math.Rand(0.5,1),
 				["eye_blink_right"] = math.Rand(0.5,1),
