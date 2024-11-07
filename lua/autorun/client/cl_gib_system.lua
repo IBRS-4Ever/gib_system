95a34eccbf3e69ccba48b674994c5d099858a101
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2024-11-07 16:11:04 +0800

    Update cl_gib_system.lua

--- lua/autorun/client/cl_gib_system.lua
+++ lua/autorun/client/cl_gib_system.lua
@@ -7,7 +7,6 @@ include("autorun/gibbing_system/convars.lua")
 hook.Add( "CreateClientsideRagdoll", "fade_out_corpses", function( entity, ragdoll )
 	if GetConVar( "gibsystem_enabled" ):GetBool() and GetConVar( "gibsystem_gibbing_npc" ):GetBool() and !(entity:IsPlayer()) and DefaultNPCs[entity:GetClass()] then
 		timer.Simple( 0.01, function()
-			if !IsValid(entity) then return end
 			if GetConVar( "developer" ):GetInt() > 0 then
 				print("Found Clientside Ragdoll: "..tostring(entity))
 			end
