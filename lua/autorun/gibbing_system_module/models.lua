107f6b9ce78b7e60517b0addc591a955a142b8ce
IBRS <56501720+IBRS-4Ever@users.noreply.github.com>
2024-12-24 01:08:26 +0800

    Update models.lua

--- lua/autorun/gibbing_system_module/models.lua
+++ lua/autorun/gibbing_system_module/models.lua
@@ -7,6 +7,7 @@ function GibSystem_LoadModels()
 	files, _ = file.Find("autorun/gibbing_system/*.lua", "LUA")
 	table.Empty(GibModels)
 	for _, filename in ipairs(files) do
+		if string.find( filename, "disabled_" ) then print( "[碎尸系统] 已跳过未使用的模型 "..tostring(filename:gsub("%.lua$", ""):gsub("disabled_", "")) ) continue end
 		AddCSLuaFile("autorun/gibbing_system/" .. filename)
 		include("autorun/gibbing_system/" .. filename)
 		table.insert(GibModels, tostring(filename:gsub("%.lua$", "")))
